eda041b864e96c185c522558228d2ba6a1a56d3f
Aleffio <gigendh@gmail.com>
2017-07-06 12:15:48 +0200

    PW-214: Added ModificationTest

---
+++ test/ModificationTest.py
@@ -0,0 +1,74 @@
+import Adyen
+import unittest
+from BaseTest import BaseTest
+
+class TestModifications(unittest.TestCase):
+    ady = Adyen.Adyen(username="user")
+
+    client = ady.client
+    test = BaseTest(ady)
+    client.username = "YourWSUser"
+    client.password = "YourWSPassword"
+    client.platform = "test"
+
+    def test_capture_success(self):
+        request = {}
+        request['merchantAccount'] = "YourMerchantAccount"
+        request['reference'] = "YourReference"
+        request['modificationAmount'] = {"value": "1234", "currency": "EUR"}
+        request['originalReference'] = "YourOriginalReference"
+        self.ady.client = self.test.create_client_from_file(200, request, 'test/mocks/capture-success.json')
+        result = self.ady.payment.capture(request)
+        self.assertEqual("[capture-received]",result.message['response'])
+
+    def test_capture_error_167(self):
+        #This test is not needed since validation.py already checks the 'originalReference' value in case it is missing
+        request = {}
+        request['merchantAccount'] = "YourMerchantAccount"
+        request['reference'] = "YourReference"
+        request['modificationAmount'] = {"value": "1234", "currency": "EUR"}
+        request['originalReference'] = "YourOriginalReference"
+        self.ady.client = self.test.create_client_from_file(422, request, 'test/mocks/capture-error-167.json')
+        self.assertRaisesRegexp(ValueError,"Received validation error with errorCode: 167, message: Original pspReference required for this operation, HTTP Code: 422." +
+                                           " Please verify the values provided. Please reach out to support@adyen.com if the problem persists, providing the PSP reference.*",
+                                self.ady.payment.capture, request)
+
+    def test_cancel_or_refund_received(self):
+        request = {}
+        request['merchantAccount'] = "YourMerchantAccount"
+        request['reference'] = "YourReference"
+        request['originalReference'] = "YourOriginalReference"
+        self.ady.client = self.test.create_client_from_file(200, request, 'test/mocks/cancelOrRefund-received.json')
+        result = self.ady.payment.cancel_or_refund(request)
+        self.assertEqual("[cancelOrRefund-received]", result.message['response'])
+
+    def test_refund_received(self):
+        request = {}
+        request['merchantAccount'] = "YourMerchantAccount"
+        request['reference'] = "YourReference"
+        request['originalReference'] = "YourOriginalReference"
+        request['modificationAmount'] = {"value": "1234", "currency": "EUR"}
+        self.ady.client = self.test.create_client_from_file(200, request, 'test/mocks/refund-received.json')
+        result = self.ady.payment.refund(request)
+        self.assertEqual("[refund-received]", result.message['response'])
+
+    def test_cancel_received(self):
+        request = {}
+        request['merchantAccount'] = "YourMerchantAccount"
+        request['reference'] = "YourReference"
+        request['originalReference'] = "YourOriginalReference"
+        self.ady.client = self.test.create_client_from_file(200, request, 'test/mocks/cancel-received.json')
+        result = self.ady.payment.cancel(request)
+        self.assertEqual("[cancel-received]", result.message['response'])
+
+TestModifications.client.http_force = "requests"
+suite = unittest.TestLoader().loadTestsFromTestCase(TestModifications)
+unittest.TextTestRunner(verbosity=2).run(suite)
+TestModifications.client.http_force = "pycurl"
+TestModifications.client.http_init = False
+suite = unittest.TestLoader().loadTestsFromTestCase(TestModifications)
+unittest.TextTestRunner(verbosity=2).run(suite)
+TestModifications.client.http_force = "other"
+TestModifications.client.http_init = False
+suite = unittest.TestLoader().loadTestsFromTestCase(TestModifications)
+unittest.TextTestRunner(verbosity=2).run(suite)
--- test/PaymentTest.py
+++ test/PaymentTest.py
@@ -1,6 +1,5 @@
 import Adyen
 import unittest
-import mock
 from BaseTest import BaseTest
 import pprint
 
@@ -12,6 +11,7 @@ class TestPayments(unittest.TestCase):
     client.username = "YourWSUser"
     client.password = "YourWSPassword"
     client.platform = "test"
+    client.http_force = "pycurl"
 
     def test_authorise_success_mocked(self):
         request = {}
@@ -27,7 +27,6 @@ class TestPayments(unittest.TestCase):
         }
         self.ady.client = self.test.create_client_from_file(200,request,'test/mocks/authorise-success.json')
         result = self.ady.payment.authorise(request)
-        print(result.message)
         self.assertEqual("Authorised", result.message['resultCode'])
         self.assertEqual("8/2018", result.message['additionalData']['expiryDate'])
         self.assertEqual("411111", result.message['additionalData']['cardBin'])
@@ -36,18 +35,14 @@ class TestPayments(unittest.TestCase):
         self.assertEqual("true", result.message['additionalData']['threeDOffered'])
         self.assertEqual("false", result.message['additionalData']['threeDAuthenticated'])
         self.assertEqual("69746", result.message['authCode'])
-
         self.assertEqual(11, len(result.message['fraudResult']['results']))
-
         fraudCheckResult = result.message['fraudResult']['results'][0]['FraudCheckResult']
         self.assertEqual("CardChunkUsage", fraudCheckResult['name'])
         self.assertEqual(8, fraudCheckResult['accountScore'])
         self.assertEquals(2, fraudCheckResult['checkId'])
 
-#Not Working ATM
     def test_authorise_error010_mocked(self):
         request = {}
-       # request['pspReference']="123123123"
         request['merchantAccount'] = "testaccount"
         request['amount'] = {"value": "100000", "currency": "EUR"}
         request['reference'] = "123456"
@@ -58,13 +53,11 @@ class TestPayments(unittest.TestCase):
             "cvc": "737",
             "holderName": "John Doe"
         }
-        self.ady.client = self.test.create_client_from_file(404,request, 'test/mocks/authorise-error-010.json')
-        result = self.ady.payment.authorise(request)
-        #print(result.message)
+        self.ady.client = self.test.create_client_from_file(403,request, 'test/mocks/authorise-error-010.json')
+        self.assertRaises(Adyen.AdyenAPIInvalidPermission,self.ady.payment.authorise,request)
 
     def test_authorise_error_cvc_declined_mocked(self):
         request = {}
-
         request['amount'] = {"value": "100000", "currency": "EUR"}
         request['reference'] = "123456"
         request['card'] = {
@@ -77,11 +70,9 @@ class TestPayments(unittest.TestCase):
         self.ady.client = self.test.create_client_from_file(200, request, 'test/mocks/authorise-error-cvc-declined.json')
         result = self.ady.payment.authorise(request)
         self.assertEqual("Refused", result.message['resultCode'])
-        print(result.message)
 
     def test_authorise_success_3d_mocked(self):
         request = {}
-
         request['amount'] = {"value": "100000", "currency": "EUR"}
         request['reference'] = "123456"
         request['card'] = {
@@ -97,11 +88,9 @@ class TestPayments(unittest.TestCase):
         self.assertIsNotNone(result.message['md'])
         self.assertIsNotNone(result.message['issuerUrl'])
         self.assertIsNotNone(result.message['paRequest'])
-        print(result.message)
 
     def test_authorise_3d_success_mocked(self):
         request = {}
-
         request['md'] = "testMD"
         request['paResponse'] = "paresponsetest"
         request['browserInfo'] = {
@@ -112,7 +101,6 @@ class TestPayments(unittest.TestCase):
         result = self.ady.payment.authorise3d(request)
         self.assertEqual("Authorised", result.message['resultCode'])
         self.assertIsNotNone(result.message['pspReference'])
-        print(result.message)
 
     def test_authorise_cse_success_mocked(self):
         request = {}
@@ -125,7 +113,6 @@ class TestPayments(unittest.TestCase):
         self.ady.client = self.test.create_client_from_file(200, request, 'test/mocks/authorise-success-cse.json')
         result = self.ady.payment.authorise(request)
         self.assertEqual("Authorised", result.message['resultCode'])
-        pprint.pprint(result.message)
 
     def test_authorise_cse_error_expired_mocked(self):
         request = {}
@@ -140,7 +127,6 @@ class TestPayments(unittest.TestCase):
         result = self.ady.payment.authorise(request)
         self.assertEqual("Refused", result.message['resultCode'])
         self.assertEqual("DECLINED Expiry Incorrect", result.message['additionalData']['refusalReasonRaw'])
-        pprint.pprint(result.message)
 
     def test_error_401_mocked(self):
         request = {}
@@ -158,5 +144,14 @@ class TestPayments(unittest.TestCase):
         self.assertRaisesRegexp(ValueError, "Unable to authenticate with Adyen's Servers. Please verify the credentials set with the Adyen base class. Please reach out to your Adyen Admin if the problem persists" , self.ady.payment.authorise, request)
 
 
-if __name__ == '__main__':
-    unittest.main()
+TestPayments.client.http_force = "requests"
+suite = unittest.TestLoader().loadTestsFromTestCase(TestPayments)
+unittest.TextTestRunner(verbosity=2).run(suite)
+TestPayments.client.http_force = "pycurl"
+TestPayments.client.http_init = False
+suite = unittest.TestLoader().loadTestsFromTestCase(TestPayments)
+unittest.TextTestRunner(verbosity=2).run(suite)
+TestPayments.client.http_force = "other"
+TestPayments.client.http_init = False
+suite = unittest.TestLoader().loadTestsFromTestCase(TestPayments)
+unittest.TextTestRunner(verbosity=2).run(suite)
