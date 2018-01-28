3826d5e21edf72cc25c8186e7b076a7de0ea7398
alina <hnalina@gmail.com>
2018-01-28 19:01:49 +0300

    not used _generate_signing_string

--- Adyen/util.py
+++ Adyen/util.py
@@ -8,25 +8,6 @@ import hashlib
 import binascii
 
 
-def _generate_signing_string(dict_object, hmac_key):
-    if not isinstance(dict_object, dict):
-        raise ValueError("Must Provide dictionary object")
-
-    def escape_val(val):
-        if isinstance(val, int):
-            return val
-        return val.replace('\\', '\\\\').replace(':', '\\:')
-
-    ordered_request = OrderedDict(sorted(dict_object.items(),
-                                         key=lambda t: t[0]))
-
-    signing_string = ':'.join(
-        map(escape_val, chain(map(str, ordered_request.keys()),
-                              map(str, ordered_request.values()))))
-
-    return signing_string
-
-
 def generate_hpp_sig(dict_object, hmac_key):
     if 'issuerId' in dict_object:
         if dict_object['issuerId'] == "":
