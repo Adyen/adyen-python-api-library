25ca3f26dcb90aece4c46512d6497154cdd54560
alexandros <alexandros.moraitis@adyen.com>
2019-01-04 10:58:33 +0100

    Format lines for checkout

--- test/CheckoutTest.py
+++ test/CheckoutTest.py
@@ -14,13 +14,10 @@ class TestCheckout(unittest.TestCase):
     client.app_name = "appname"
 
     def test_payment_methods_success_mocked(self):
-        request = {}
-        request['merchantAccount'] = "YourMerchantAccount"
-
+        request = {'merchantAccount': "YourMerchantAccount"}
+        file = "test/mocks/checkout/paymentmethods-success.json"
         self.ady.client = self.test.create_client_from_file(200, request,
-                                                            'test/mocks/checkout/'
-                                                            'paymentmethods-success'
-                                                            '.json')
+                                                            file)
         result = self.ady.checkout.payment_methods(request)
         self.assertEqual("AliPay", result.message['paymentMethods'][0]['name'])
         self.assertEqual("Credit Card", result.message['paymentMethods'][2]['name'])
