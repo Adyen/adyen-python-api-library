25afd0cdb117f18036ce8a905323bfda45413d70
alexandros <alexandros.moraitis@adyen.com>
2019-01-15 15:02:10 +0100

    remove ENDPOINT_LIVE_SUFFIX for pal

--- Adyen/settings.py
+++ Adyen/settings.py
@@ -2,7 +2,6 @@
 
 BASE_PAL_URL = "https://pal-{}.adyen.com/pal/servlet"
 BASE_HPP_URL = "https://{}.adyen.com/hpp"
-ENDPOINT_LIVE_SUFFIX = "-pal-live.adyenpayments.com"
 ENDPOINT_CHECKOUT_TEST = "https://checkout-test.adyen.com"
 ENDPOINT_CHECKOUT_LIVE_SUFFIX = "https://{}-checkout-live" \
                                 ".adyenpayments.com/checkout"
