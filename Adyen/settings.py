19dc69d6367c396cd745d2cbf10e1fffcbf2a824
alexandros <alexandros.moraitis@adyen.com>
2019-02-11 15:15:37 +0100

    Update the version to v41

--- Adyen/settings.py
+++ Adyen/settings.py
@@ -4,7 +4,7 @@ BASE_HPP_URL = "https://{}.adyen.com/hpp"
 ENDPOINT_CHECKOUT_TEST = "https://checkout-test.adyen.com"
 ENDPOINT_CHECKOUT_LIVE_SUFFIX = "https://{}-checkout-live" \
                                 ".adyenpayments.com/checkout"
-API_CHECKOUT_VERSION = "v40"
+API_CHECKOUT_VERSION = "v41"
 API_CHECKOUT_UTILITY_VERSION = "v1"
 API_RECURRING_VERSION = "v25"
 API_PAYMENT_VERSION = "v40"
