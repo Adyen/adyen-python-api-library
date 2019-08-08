482a0266145f564f7c7c8d3754d30d13bb62f973
Alexandros Moraitis <alexandros.moraitis@adyen.com>
2019-08-08 16:05:02 +0200

    Fix payout tests (#74)
    * Fix payout tests
    
    * update travis file
    
    * update travis file
    
    * upgrade pip for CI
    
    * travis file add update before install
    
    * Update .travis.yml

--- Adyen/client.py
+++ Adyen/client.py
@@ -233,7 +233,7 @@ class AdyenClient(object):
             username = self.username
         elif 'username' in kwargs:
             username = kwargs.pop("username")
-        elif service == "Payout":
+        if service == "Payout":
             if any(substring in action for substring in
                    ["store", "submit"]):
                 username = self._store_payout_username(**kwargs)
@@ -254,7 +254,7 @@ class AdyenClient(object):
             password = self.password
         elif 'password' in kwargs:
             password = kwargs.pop("password")
-        elif service == "Payout":
+        if service == "Payout":
             if any(substring in action for substring in
                    ["store", "submit"]):
                 password = self._store_payout_pass(**kwargs)
