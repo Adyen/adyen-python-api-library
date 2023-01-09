6a9657ca6e2b4d24462485b1323f8c59b1159a09
Antoni Stroinski <55943882+antolo-arch@users.noreply.github.com>
2023-01-09 16:41:19 +0100

    removed unnecessary functions

--- Adyen/util.py
+++ Adyen/util.py
@@ -1,51 +1,9 @@
 from __future__ import absolute_import, division, unicode_literals
 
-from itertools import chain
-from collections import OrderedDict
 import base64
 import hmac
 import hashlib
 import binascii
-
-
-def generate_hpp_sig(dict_object, hmac_key):
-    if 'issuerId' in dict_object:
-        if dict_object['issuerId'] == "":
-            del dict_object['issuerId']
-
-    if not isinstance(dict_object, dict):
-        raise ValueError("Must Provide dictionary object")
-
-    def escape_val(val):
-        if isinstance(val, int):
-            return val
-        return val.replace('\\', '\\\\').replace(':', '\\:')
-
-    hmac_key = binascii.a2b_hex(hmac_key)
-
-    ordered_request = OrderedDict(sorted(dict_object.items(),
-                                         key=lambda t: t[0]))
-
-    signing_string = ':'.join(
-        map(escape_val, chain(map(str, ordered_request.keys()),
-                              map(str, ordered_request.values()))))
-
-    hm = hmac.new(hmac_key, signing_string.encode('utf-8'), hashlib.sha256)
-    return base64.b64encode(hm.digest())
-
-
-def is_valid_hmac(dict_object, hmac_key):
-    dict_object = dict_object.copy()
-
-    if 'additionalData' in dict_object:
-        if dict_object['additionalData']['hmacSignature'] == "":
-            raise ValueError("Must Provide hmacSignature in additionalData")
-        else:
-            expected_sign = dict_object['additionalData']['hmacSignature']
-            del dict_object['additionalData']
-            merchant_sign = generate_hpp_sig(dict_object, hmac_key)
-            merchant_sign_str = merchant_sign.decode("utf-8")
-            return hmac.compare_digest(merchant_sign_str, expected_sign)
 
 
 def generate_notification_sig(dict_object, hmac_key):
@@ -55,11 +13,6 @@ def generate_notification_sig(dict_object, hmac_key):
 
     if not isinstance(dict_object, dict):
         raise ValueError("Must Provide dictionary object")
-
-    def escape_val(val):
-        if isinstance(val, int):
-            return val
-        return val.replace('\\', '\\\\')
 
     hmac_key = binascii.a2b_hex(hmac_key)
 
@@ -78,9 +31,7 @@ def generate_notification_sig(dict_object, hmac_key):
         'success',
     ]
 
-    signing_string = ':'.join(
-        map(escape_val, map(str, (
-            request_dict.get(element, '') for element in element_orders))))
+    signing_string = ':'.join(map(str, (request_dict.get(element, '') for element in element_orders)))
 
     hm = hmac.new(hmac_key, signing_string.encode('utf-8'), hashlib.sha256)
     return base64.b64encode(hm.digest())
