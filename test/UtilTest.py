50baea2796787f71d72064aca8aa43c08f967608
Antoni Stroinski <55943882+antolo-arch@users.noreply.github.com>
2023-01-10 09:26:54 +0100

    fix unit tests

--- test/UtilTest.py
+++ test/UtilTest.py
@@ -2,8 +2,6 @@ import unittest
 
 import Adyen
 from Adyen.util import (
-    generate_hpp_sig,
-    is_valid_hmac,
     generate_notification_sig,
     is_valid_hmac_notification,
     get_query
@@ -13,32 +11,6 @@ from Adyen.util import (
 class UtilTest(unittest.TestCase):
     ady = Adyen.Adyen()
     client = ady.client
-
-    def test_hpp_request_item_hmac(self):
-        request = {
-            "pspReference": "pspReference",
-            "originalReference": "originalReference",
-            "merchantAccount": "merchantAccount",
-            "amount": {
-                "currency": "EUR",
-                "value": 100000
-            },
-            "eventCode": "EVENT",
-            "Success": "true"
-        }
-        key = "DFB1EB5485895CFA84146406857104AB" \
-              "B4CBCABDC8AAF103A624C8F6A3EAAB00"
-        hmac_calculation = generate_hpp_sig(request, key)
-        hmac_calculation_str = hmac_calculation.decode("utf-8")
-        expected_hmac = "+xK25vgc9XcZFwu7WNLIwqVewyumVsgp+X+C0a2e+DE="
-        self.assertTrue(hmac_calculation_str != "")
-        self.assertEqual(hmac_calculation_str, expected_hmac)
-        request['additionalData'] = {'hmacSignature': hmac_calculation_str}
-        hmac_validate = is_valid_hmac(request, key)
-        self.assertIn('additionalData', request)
-        self.assertDictEqual(request['additionalData'],
-                             {'hmacSignature': hmac_calculation_str})
-        self.assertTrue(hmac_validate)
 
     def test_notification_request_item_hmac(self):
         request = {
