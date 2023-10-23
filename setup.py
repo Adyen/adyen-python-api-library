cbe0217e0ebe7433d8f75b18901a1a8bcdc9e7bb
Adyen Automation <plugins_dev@adyen.com>
2023-10-23 14:32:41 +0000

    chore(release): bump to 10.0.0-beta

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Adyen',
     packages=find_packages(include="Adyen*"),
-    version='9.1.0',
+    version='10.0.0-beta',
     maintainer='Adyen',
     maintainer_email='support@adyen.com',
     description='Adyen Python Api',
