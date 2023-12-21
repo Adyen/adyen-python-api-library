20af6222e777c907f82679ed0d187ea6452312df
Alexandros Moraitis <alexandros.moraitis@adyen.com>
2023-12-21 09:07:27 +0000

    remove extra check

--- Adyen/util.py
+++ Adyen/util.py
@@ -47,8 +47,7 @@ def is_valid_hmac_notification(dict_object, hmac_key):
             raise ValueError("Must Provide hmacSignature in additionalData")
         else:
             expected_sign = dict_object['additionalData']['hmacSignature']
-            if 'additionalData' in dict_object:
-                del dict_object['additionalData']
+            del dict_object['additionalData']
             merchant_sign = generate_notification_sig(dict_object, hmac_key)
             merchant_sign_str = merchant_sign.decode("utf-8")
             return hmac.compare_digest(merchant_sign_str, expected_sign)
