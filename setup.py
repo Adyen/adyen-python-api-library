68486bc0a94d4289c90742bbddfe72570ca993d1
DjoykeAbyah <DjoykeAbyah@users.noreply.github.com>
2024-02-22 12:05:43 +0000

    chore(release): bump to 11.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Adyen',
     packages=find_packages(include="Adyen*"),
-    version='11.1.0',
+    version='11.2.0',
     maintainer='Adyen',
     maintainer_email='support@adyen.com',
     description='Adyen Python Api',
